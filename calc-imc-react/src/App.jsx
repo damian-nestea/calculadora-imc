a56fbdfc2fe442a2b603ef27154e1ddc6a6160c3
damian-nestea <damian@hotmail.cz>
2023-05-07 15:31:07 -0300

    feature adição da estrutura left side do projeto com título, parágrafo e inputs para calculadora IMC com estados linkados nos inputs

--- calc-imc-react/src/App.jsx
+++ calc-imc-react/src/App.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 import Logo from "./assets/logo.png";
 
 const App = () => {
+  const [heightInput, setHeightInput] = useState(0);
+  const [weightInput, setWeightInput] = useState(0);
+
+  const handleCalculateIMC = () => {
+    if (heightInput && weightInput) {
+    } else {
+      alert("Digite todos os campos!");
+    }
+  };
   return (
     <div className={styles.mainAppContainer}>
       <header className={styles.headerContainer}>
         <img src={Logo} />
       </header>
       <main>
-        <section className={styles.leftSideContainer}>Left</section>
+        <section className={styles.leftSideContainer}>
+          <h1>Calculadora de IMC</h1>
+          <p>
+            IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada
+            pessoa.
+          </p>
+          <p>
+            Que tal descobrir o seu IMC? Coloque seus dados na calculadora!.
+          </p>
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 183 (em centímetros)."
+            value={heightInput > 0 ? heightInput : ""}
+            onChange={(e) => {
+              setHeightInput(e.target.value);
+            }}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 80.3 (em kg)."
+            value={weightInput > 0 ? weightInput : ""}
+            onChange={(e) => {
+              setWeightInput(e.target.value);
+            }}
+          />
+          <button onClick={handleCalculateIMC}>Calcular</button>
+        </section>
         <section className={styles.rightSideContainer}>Right</section>
       </main>
     </div>
