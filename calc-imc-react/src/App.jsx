b9cad08b18a6c26d194ae01662fbe55291cec0a8
damian-nestea <damian@hotmail.cz>
2023-05-07 15:09:08 -0300

    feature criação de estrutura básica do corpo do site, com left e right side

--- calc-imc-react/src/App.jsx
+++ calc-imc-react/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./App.module.css";
-import Logo from './assets/logo.png'
+import Logo from "./assets/logo.png";
 
 const App = () => {
-  return <div className={styles.mainAppContainer}>
-    <header className={styles.headerContainer}>
-      <img src={Logo}/>  
-    </header></div>;
+  return (
+    <div className={styles.mainAppContainer}>
+      <header className={styles.headerContainer}>
+        <img src={Logo} />
+      </header>
+      <main>
+        <section className={styles.leftSideContainer}>Left</section>
+        <section className={styles.rightSideContainer}>Right</section>
+      </main>
+    </div>
+  );
 };
 
 export default App;
