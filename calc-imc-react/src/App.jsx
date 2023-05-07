acaaca2c5dce188526062b86ac0be6949e7dda59
damian-nestea <damian@hotmail.cz>
2023-05-07 15:02:19 -0300

    feature adição de logo, criação de header com logo, alinhamento e efeito hover

--- calc-imc-react/src/App.jsx
+++ calc-imc-react/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "./App.module.css";
+import Logo from './assets/logo.png'
 
 const App = () => {
-  return <div className={styles.mainAppContainer}>Application</div>;
+  return <div className={styles.mainAppContainer}>
+    <header className={styles.headerContainer}>
+      <img src={Logo}/>  
+    </header></div>;
 };
 
 export default App;
