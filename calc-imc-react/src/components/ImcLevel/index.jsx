9fa4fd80b383d06fcf110bb8fb98586b6b4f016a
damian-nestea <damian@hotmail.cz>
2023-05-08 22:44:15 -0300

    feature adição de icone no level de imc, com título e descrição de range do índice, alinhamento e estilização

--- calc-imc-react/src/components/ImcLevel/index.jsx
+++ calc-imc-react/src/components/ImcLevel/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./ImcLevel.module.css";
+import happyIcon from "../../assets/happy.png"
+import sadIcon from "../../assets/sad.png"
 
 const ImcLevel = ({ level }) => {
   return (
     <div className={styles.flexItem} style={{ backgroundColor: level.color}}>
+      <img className={styles.icone} src={level.icon === 'happy' ? happyIcon : sadIcon } />
       <h2 className={styles.titulo}>{level.level}</h2>
-      
+      <p className={styles.descricao}>IMC entre <strong>{level.imc[0]}</strong> e <strong>{level.imc[1]}</strong>.</p>
     </div>
   );
 };
